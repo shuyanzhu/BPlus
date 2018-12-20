19cc22300aadf798eabb2732cb40e063237e9a36
eureka <1294206499@qq.com>
2018-12-20 16:20:02 +0800

    一般提交

--- main.cc
+++ main.cc
@@ -6,15 +6,21 @@
  ************************************************************************/
 
 #include <iostream>
+#include <time.h>
 
 #include "include/vector/vector.h"
 #include "include/BPT.h"
 using namespace std;
 
-int main() {
-    BPT<int> test;
-    for(int i = 0; i < 6; i++){
+int main()
+{
+    clock_t t1, t2;
+    t1 = clock();
+    BPT<int> test(20);
+    for (int i = 0; i < 5000000; i++) {
         test.insert(i);
     }
+    t2 = clock();
+    printf("时间：%ld\n", (t2 - t1) / CLOCKS_PER_SEC);
     return 1;
 }
