325719e0b188e011c6e7e8e1b27dbe5354632250
eureka <1294206499@qq.com>
2018-12-22 17:01:41 +0800

    一般提交

--- main.cc
+++ main.cc
@@ -10,38 +10,37 @@
 #include <stdlib.h>
 
 #include "include/vector/vector.h"
-#include"include/BPT.h"
+#include "include/BPT.h"
 using namespace std;
 
 int main()
 {
+    BPT<int> test(5);
+    volatile clock_t t1, t2;
+    t1 = clock();
 
-	BPT<int> test(5);
-	volatile clock_t t1, t2;
-	t1 = clock();
+    //// 顺序/倒序插入速度测试
+    // for (int i = 0; i < 10000000; i++) {
+    //    test.insert(i);
+    //}
+    // for (int i = 0; i < 10000000; i++) {
+    // test.insert(10000000-i);
+    //}
 
-	//// 顺序/倒序插入速度测试
-	//for (int i = 0; i < 10000000; i++) {
-	//    test.insert(i);
-	//}
-	//for (int i = 0; i < 10000000; i++) {
-	// test.insert(10000000-i);
-	//}
+    // 乱序插入、删除正确性测试，速度测试
+    srand(1);
+    for (int i = 0; i < 10000000; i++) {
+        test.insert(rand() * rand());
+    }
+    test.print(0);
+    test.search(17400);
+    for (int i = 0; i < 1000000; i++) {
+        % test.remove(rand());
+    }
+    test.print(0);
 
-	// 乱序插入、删除正确性测试，速度测试
-	srand(1);
-	for (int i = 0; i < 10000000; i++) {
-		test.insert(rand()*rand());
-	}
-	//test.print(0);
-	//test.search(17400);
-	//for (int i = 0; i < 1000000; i++) {%
-	//	test.remove(rand());
-	//}
-	//test.print(0);
-
-	t2 = clock();
-	printf("time: %ld\n%d", (t2 - t1) / CLOCKS_PER_SEC, test.h);
-	//system("pause");
-	return 0;
+    t2 = clock();
+    printf("time: %ld\n%d", (t2 - t1) / CLOCKS_PER_SEC, test.h);
+    // system("pause");
+    return 0;
 }
