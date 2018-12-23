5724647b758c43b4c731efafcdf694354efb2ed0
eureka <1294206499@qq.com>
2018-12-23 14:24:51 +0800

    一般提交

--- include/BPTNode.h
+++ include/BPTNode.h
@@ -1,22 +1,15 @@
 #pragma once
 
-#include "vector/vector.h"
-//#include "Array.h"
+//#include "vector/vector.h"
+#include "Array.h"
 template <typename T>
-struct BPTNode
-{
+struct BPTNode {
     BPTNode<T> *parent;
-    Vector<T> key;
-    Vector<BPTNode<T> *> child;
-    // Array<T> key;
-    // Array<BPTNode<T> *> child;
+  //  Vector<T> key;
+   // Vector<BPTNode<T> *> child;
+	Array<T> key;
+	Array<BPTNode<T> *> child;
     BPTNode<T> *prev, *next;
     // 默认构造函数只在根节点创建时被调用
-    BPTNode()
-        : parent(NULL)
-        , prev(NULL)
-        , next(NULL)
-    {}
-    // BPTNode(int order):key(order+1), child(order+1), parent(NULL),
-    // prev(NULL), next(NULL) {}
+	BPTNode(int order):key(order+1), child(order+1), parent(NULL), prev(NULL), next(NULL) {}
 };
