4091939255992c9186deeac08ff9dfbc534692e1
zhuhuiming <1294206499@qq.com>
2018-12-23 13:46:47 +0800

    Array重构

--- include/BPTNode.h
+++ include/BPTNode.h
@@ -1,19 +1,15 @@
 #pragma once
 
-#pragma once
-#include "vector/vector.h"
-
+//#include "vector/vector.h"
+#include "Array.h"
 template <typename T>
 struct BPTNode {
     BPTNode<T> *parent;
-    Vector<T> key;
-    Vector<BPTNode<T> *> child;
+  //  Vector<T> key;
+   // Vector<BPTNode<T> *> child;
+	Array<T> key;
+	Array<BPTNode<T> *> child;
     BPTNode<T> *prev, *next;
     // 默认构造函数只在根节点创建时被调用
-	BPTNode() :parent(NULL), prev(NULL), next(NULL) {}
-//    BPTNode(T e, BPTNode<T> *child=NULL){
-//        parent = NULL;
-//        key.insert(0, e);
-//    }
-
+	BPTNode(int order):key(order+1), child(order+1), parent(NULL), prev(NULL), next(NULL) {}
 };
